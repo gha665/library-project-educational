807679820790da02a5631c12f6768321e60a1c85
Evgeny Galitskiy <gha665@gmail.com>
2020-01-28 23:59:26 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+//create a route "book/add"
+router.get('/books/add', (req, res, next) => {
+  res.render("book-add");
+});
+
+router.post('/books/add', (req, res, next) => {
+  
+});
+
+
 module.exports = router;
